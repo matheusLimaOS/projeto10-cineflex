69a225b64b43162eabb011f5ed056b76a79af8c5
Matheus Lima <69645734+matheusLimaOS@users.noreply.github.com>
2022-10-10 14:40:55 -0300

    Adição correção automática

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,11 +4,11 @@ export default function Form(props) {
             <label>
                 Nome do comprador do assento {props.selecionado}:
             </label>
-            <input type='text' name={props.selecionado+'nome'} placeholder="Digite seu nome..." required />
+            <input data-identifier="buyer-name-input" type='text' name={props.selecionado+'nome'} placeholder="Digite seu nome..." required />
             <label>
                 CPF do comprador do assento {props.selecionado}:
             </label>
-            <input type='number' name={props.selecionado+'cpf'} onChange={handleChange} placeholder="Digite seu CPF..." required />
+            <input data-identifier="buyer-cpf-input" type='number' name={props.selecionado+'cpf'} onChange={handleChange} placeholder="Digite seu CPF..." required />
         </>
     )
 }
